26ed35dea10a78d3d1be6e3bcb097db2c4c04f8d
EbramIbrahim <abramibrahem22@gmail.com>
2024-03-28 15:22:01 +0200

    setup-data

--- app/src/main/java/com/example/solutionxarch/core/domain/contracts/RemoteDataStoreMiddleWareProvider.kt
+++ app/src/main/java/com/example/solutionxarch/core/domain/contracts/RemoteDataStoreMiddleWareProvider.kt
@@ -2,21 +2,23 @@ package com.example.solutionxarch.core.domain.contracts
 
 import retrofit2.http.HeaderMap
 import retrofit2.http.QueryMap
+import retrofit2.http.Url
 
 class RemoteDataStoreMiddleWareProvider() {
 
     @JvmOverloads
-    fun <RESPONSE, P> get(
-        endpoint: String,
-        @QueryMap queries: Map<String, P>? = null,
-        @HeaderMap headers: Map<String, P>? = null,
+    fun <RESPONSE, PARAMS> get(
+        @Url endpoint: String,
+        @QueryMap queries: Map<String, PARAMS>? = null,
+        @HeaderMap headers: Map<String, PARAMS>? = null,
     ): RESPONSE {
         TODO()
     }
 
-    fun <RESPONSE, P> post(
-        endpoint: String,
-        headers: Map<String, P>? = null,
+    fun <RESPONSE, PARAMS> post(
+        @Url endpoint: String,
+        @QueryMap queries: Map<String, PARAMS>? = null,
+        @HeaderMap headers: Map<String, PARAMS>? = null,
     ): RESPONSE {
         TODO()
     }
